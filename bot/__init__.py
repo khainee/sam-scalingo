4aac16c7e46fe064433f397d332d942dcd8315f2
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-04-10 22:12:40 +0630

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -309,6 +309,11 @@ if len(LEECH_SPLIT_SIZE) == 0 or int(LEECH_SPLIT_SIZE) > TG_MAX_FILE_SIZE:
 else:
     LEECH_SPLIT_SIZE = int(LEECH_SPLIT_SIZE)
 
+RC_INDEX_PORT = environ.get('PORT', '')
+BASE_URL = environ.get('BASE_URL', '')
+RC_INDEX_USER = environ.get('RC_INDEX_USER', '')
+RC_INDEX_PASS environ.get('RC_INDEX_PASS', '')
+
 if not config_dict:
     config_dict = {'AS_DOCUMENT': AS_DOCUMENT,
                    'ALLOWED_CHATS': ALLOWED_CHATS,
